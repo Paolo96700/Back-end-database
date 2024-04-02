ea2cebed8b118545752a74c4a7ab194918cce658
Paolo Falco <paolo.falcoapp@gmail.com>
2024-04-02 11:39:42 +0200

    Revert "add migration"
    This reverts commit 3c1d8672169b27db3db4e6b75726c55c18660c67.

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Photo;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -18,8 +17,3 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Artisan::command('photos', function(){
-    $photos = Photo::all('id', 'title', 'visible', 'user_id');
-    $this->table(['ID', 'Titolo', 'Visibilità', 'Fotografo'], $photos->toArray());
-})->purpose('Command that you can all photos');
